1beb7173050e98cc35ea47942ca6403d1c955bea
GowthamGoush <westcity.21.gowtham@gmail.com>
2022-09-25 15:11:24 +0530

    Update socket methods

--- lib/utils/socket_methods.dart
+++ lib/utils/socket_methods.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:type_battle/utils/socket_client.dart';
+import '../providers/client_state_provider.dart';
+import '../providers/game_state_provider.dart';
 
 class SocketMethods {
   final _socketClient = SocketClient.instance.socket!;
@@ -30,5 +32,75 @@ class SocketMethods {
       'userInput': value,
       'gameID': gameID,
     });
+  }
+
+  // listeners
+  updateGameListener(BuildContext context) {
+    _socketClient.on('updateGame', (data) {
+      final gameStateProvider =
+          Provider.of<GameStateProvider>(context, listen: false)
+              .updateGameState(
+        id: data['_id'],
+        players: data['players'],
+        isJoin: data['isJoin'],
+        words: data['words'],
+        isOver: data['isOver'],
+      );
+
+      if (data['_id'].isNotEmpty && !_isPlaying) {
+        Navigator.pushNamed(context, '/game-screen');
+        _isPlaying = true;
+      }
+    });
+  }
+
+  startTimer(playerId, gameID) {
+    _socketClient.emit(
+      'timer',
+      {
+        'playerId': playerId,
+        'gameID': gameID,
+      },
+    );
+  }
+
+  notCorrectGameListener(BuildContext context) {
+    _socketClient.on(
+      'notCorrectGame',
+      (data) => ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(data),
+        ),
+      ),
+    );
+  }
+
+  updateTimer(BuildContext context) {
+    final clientStateProvider =
+        Provider.of<ClientStateProvider>(context, listen: false);
+    _socketClient.on('timer', (data) {
+      clientStateProvider.setClientState(data);
+    });
+  }
+
+  updateGame(BuildContext context) {
+    _socketClient.on('updateGame', (data) {
+      final gameStateProvider =
+          Provider.of<GameStateProvider>(context, listen: false)
+              .updateGameState(
+        id: data['_id'],
+        players: data['players'],
+        isJoin: data['isJoin'],
+        words: data['words'],
+        isOver: data['isOver'],
+      );
+    });
+  }
+
+  gameFinishedListener() {
+    _socketClient.on(
+      'done',
+      (data) => _socketClient.off('timer'),
+    );
   }
 }
